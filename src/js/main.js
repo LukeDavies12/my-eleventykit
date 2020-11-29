06003c1c336de8624cfe93860afef262cd7d43aa
LukeDavies12 <luketdavies12@gmail.com>
2020-11-29 10:51:18 -0600

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Import our Tailwind CSS file with directives and any
+ * custom css rules
+ */
+import '../css/style.css'
+
+/**
+ * Import Alpine JS
+ */
+import 'alpinejs'
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    purge: ['./src/**/*.liquid'],
+    theme: {},
+    variants: {},
+    plugins: [
+        require('@tailwindcss/typography')
+    ],
+}
